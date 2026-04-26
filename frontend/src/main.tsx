67d4281e8c133d16aa53af7a160c9a3314e6fa73
lxakshatseth <lxakshatseth90@gmail.com>
2026-04-26 11:13:25 +0530

    feat: final frontend updates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App";
-  import "./styles/index.css";
+import { createRoot } from "react-dom/client";
+import App from "./app/App";
+import "../app/globals.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
   
